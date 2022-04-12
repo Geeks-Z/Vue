cff0db8f21f6408c2cf60d926d69eeafc0fd299d
Geeks_Z <izhw1024@163.com>
2022-04-12 15:11:16 +0800

    loader-update

---
+++ Code/13-webpack的使用/03-webpackLoader/src/js/mathUtils.js
@@ -0,0 +1,20 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: Geeks_Z
+ * @Date: 2022-04-12 12:34:38
+ * @LastEditors: Geeks_Z
+ * @LastEditTime: 2022-04-12 12:34:39
+ */
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function mul(num1, num2) {
+  return num1 * num2
+}
+
+module.exports = {
+  add,
+  mul
+}
---
+++ Code/13-webpack的使用/03-webpackLoader/src/main.js
@@ -0,0 +1,28 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: Geeks_Z
+ * @Date: 2022-04-12 12:34:47
+ * @LastEditors: Geeks_Z
+ * @LastEditTime: 2022-04-12 13:07:24
+ */
+// 1.使用commonjs的模块化规范
+const {add, mul} = require('./js/mathUtils.js')
+
+console.log(add(20, 30));
+console.log(mul(20, 30));
+
+// 2.使用ES6的模块化的规范
+import {name, age, height} from "./js/info";
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+// 3.依赖css文件
+require('./css/normal.css')
+
+// 4.依赖less文件
+// require('./css/special.less')
+// document.writeln('<h2>你好啊,李银河!</h2>')
+
