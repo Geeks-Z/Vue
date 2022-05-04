877abb8be404d6dc3b56c8f107d459d01b4e5a2e
Geeks_Z <izhw1024@163.com>
2022-05-04 17:46:57 +0800

    shangguigu_app

---
+++ Code/19-Axios/learnaxios/learnaxios/src/App.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: Geeks_Z
+ * @Date: 2022-04-28 19:59:04
+ * @LastEditors: Geeks_Z
+ * @LastEditTime: 2022-04-28 20:00:55
+-->
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Code/Manage/manage/src/common/utils.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0
+ * @Author: Geeks_Z
+ * @Date: 2022-04-28 21:53:02
+ * @LastEditors: Geeks_Z
+ * @LastEditTime: 2022-04-28 21:53:02
+ */
